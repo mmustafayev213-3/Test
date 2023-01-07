93f2fcd75b02d7ec3b51b2fb07822976ecd1de00
MahammadMustafayev <tu7ekdm78@code.edu.az>
2023-01-07 12:03:08 +0400

    added

---
+++ Odeme/Odeme/Models/Commerce.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odeme.Models
+{
+    internal class Commerce
+    {
+        public DateTime OneHour { get; set; }
+        public int HourValue { get; set; }
+        public int ThreeHour { get; set; }
+        //public int ThreeHourValue { get; set; }
+        public int FiveHour { get; set; }
+        //public int FiveHourValue { get; set; }
+        public double TotalMoney { get; set; }
+        public void Payment()
+        {
+            if (TotalMoney>=HourValue)
+            {
+                Console.WriteLine($"Sizin secidiyiniz pakete gore {HourValue} bu qeder miqdar cixilacaq ");
+                TotalMoney = TotalMoney - HourValue;
+                
+                Console.WriteLine($"Umumi mebleq:{TotalMoney}");
+            }
+            else
+            {
+                Console.WriteLine("Pulunuz Catmir");
+            }
+        }
+    }
+}
---
+++ Odeme/Odeme/Program.cs
@@ -0,0 +1,59 @@
+﻿using Odeme.Models;
+
+namespace Odeme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Commerce commerce = new Commerce();
+            int choise;
+                Console.WriteLine("1-Saatlik paket ucun '1'-secin");
+                Console.WriteLine("2-Saatlik paket ucun '2'-secin");
+                Console.WriteLine("3-Saatlik paket ucun '3'-secin");
+                Console.WriteLine("4-Cixis");
+                choise= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Umumi mebleqi daxil edin:");
+                commerce.TotalMoney= Convert.ToDouble(Console.ReadLine());
+                switch (choise)
+                {
+                    case 1:
+                        commerce.HourValue = 1;
+                        //commerce.Payment();
+                    commerce.OneHour=DateTime.Now.AddMinutes(20);
+                    Console.WriteLine("==================");
+                    Console.WriteLine(commerce.OneHour.Minute);
+                    
+                    DateTime time = DateTime.Now;
+                    //DateTime time= commerce.OneHour.AddMinutes(5);
+                    Console.WriteLine(time.Minute);
+                    if (commerce.OneHour.Minute==DateTime.Now.Minute)
+                    {
+                        Console.WriteLine("Sizin Vaxtiniz bitmisdir");
+                    }
+                    else
+                    {
+                        Console.WriteLine("sizin hele vatiniz var");
+                    }
+                    //Console.WriteLine(time.ToLocalTime());
+                        break;
+                    case 2:
+                        commerce.HourValue = 3;
+                        commerce.Payment();
+                    commerce.ThreeHour = 180;
+                        break;
+                    case 3:
+                        commerce.HourValue = 5;
+                        commerce.Payment();
+                    commerce.FiveHour = 300;
+                            break; 
+                    case 4:
+                        Console.WriteLine("Saqolun");
+                        break;
+                    default:
+                        Console.WriteLine("Bele secim yoxdur");
+                        break;
+                }
+        }
+    }
+}
